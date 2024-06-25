acc4ff281a93f4c4860b83e6b0084904891155a6
Agusilham1 <173189798+Agusilham1@users.noreply.github.com>
2024-06-26 02:57:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,11 @@
 /usr/sbin/sshd
 
 # Print container's IP address
-ip addr show eth0 | grep "inet\b" | awk '{print "Container IP: " $2}' | cut -d/ -f1
+if command -v ip > /dev/null 2>&1; then
+    ip addr show eth0 | grep "inet\b" | awk '{print "Container IP: " $2}' | cut -d/ -f1
+else
+    echo "ip command not found"
+fi
 
 # Keep the container running
 tail -f /dev/null
